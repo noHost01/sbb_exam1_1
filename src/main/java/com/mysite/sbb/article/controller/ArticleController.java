5e27aef2ecc51dfd214851c67b21e228c519d6e6
noHost01 <ljh12081308@naver.com>
2022-07-06 13:27:41 +0900

    리스트 1개 찾기

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -5,9 +5,12 @@ import com.mysite.sbb.article.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/usr/article")
 public class ArticleController {
@@ -24,5 +27,12 @@ public class ArticleController {
     @ResponseBody
     public List<Article> showList() {
         return articleRepository.findAll();
+    }
+
+    @RequestMapping("/detail")
+    @ResponseBody
+    public Article showDetail(@RequestParam long id) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.orElse(null);
     }
 }
